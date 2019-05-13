7433b73de8cc537e082465b2a14b3dbefa5adfd5
ryczbo <45493100+ryczbo@users.noreply.github.com>
2019-05-13 22:51:32 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,64 +1,57 @@
 
-	let pictures = [];
-	let guesses = [];
-	let attempts = 0;
-	let score = 0;
-	let count = 0;
+	let pictures = [ "http://img5.uploadhouse.com/fileuploads/17699/176992640c06707c66a5c0b08a2549c69745dc2c.png",
+	"http://img6.uploadhouse.com/fileuploads/17699/17699263b01721074bf094aa3bc695aa19c8d573.png",
+	"http://img6.uploadhouse.com/fileuploads/17699/17699262833250fa3063b708c41042005fda437d.png",
+	"http://img9.uploadhouse.com/fileuploads/17699/176992615db99bb0fd652a2e6041388b2839a634.png",
+	"http://img4.uploadhouse.com/fileuploads/17699/176992601ca0f28ba4a8f7b41f99ee026d7aaed8.png",
+	"http://img3.uploadhouse.com/fileuploads/17699/17699259cb2d70c6882adc285ab8d519658b5dd7.png",
+	"http://img2.uploadhouse.com/fileuploads/17699/1769925824ea93cbb77ba9e95c1a4cec7f89b80c.png",
+	"http://img7.uploadhouse.com/fileuploads/17699/1769925708af4fb3c954b1d856da1f4d4dcd548a.png",
+	"http://img9.uploadhouse.com/fileuploads/17699/176992568b759acd78f7cbe98b6e4a7baa90e717.png",
+	"http://img9.uploadhouse.com/fileuploads/17699/176992554c2ca340cc2ea8c0606ecd320824756e.png",
+	"http://img5.uploadhouse.com/fileuploads/17699/176992640c06707c66a5c0b08a2549c69745dc2c.png",
+	"http://img6.uploadhouse.com/fileuploads/17699/17699263b01721074bf094aa3bc695aa19c8d573.png",
+	"http://img6.uploadhouse.com/fileuploads/17699/17699262833250fa3063b708c41042005fda437d.png",
+	"http://img9.uploadhouse.com/fileuploads/17699/176992615db99bb0fd652a2e6041388b2839a634.png",
+	"http://img4.uploadhouse.com/fileuploads/17699/176992601ca0f28ba4a8f7b41f99ee026d7aaed8.png",
+	"http://img3.uploadhouse.com/fileuploads/17699/17699259cb2d70c6882adc285ab8d519658b5dd7.png",
+	"http://img2.uploadhouse.com/fileuploads/17699/1769925824ea93cbb77ba9e95c1a4cec7f89b80c.png",
+	"http://img7.uploadhouse.com/fileuploads/17699/1769925708af4fb3c954b1d856da1f4d4dcd548a.png",
+	"http://img9.uploadhouse.com/fileuploads/17699/176992568b759acd78f7cbe98b6e4a7baa90e717.png",
+	"http://img9.uploadhouse.com/fileuploads/17699/176992554c2ca340cc2ea8c0606ecd320824756e.png"];
+	let attempts = document.getElementById('counter');
+	let firstCard;
+	let secondCard;
 	let board = '';
 	let innerSrc = '';
 	let hasChecked = false;
-	let picturesImgs = [
-  "http://img5.uploadhouse.com/fileuploads/17699/176992640c06707c66a5c0b08a2549c69745dc2c.png",
-  "http://img6.uploadhouse.com/fileuploads/17699/17699263b01721074bf094aa3bc695aa19c8d573.png",
-  "http://img6.uploadhouse.com/fileuploads/17699/17699262833250fa3063b708c41042005fda437d.png",
-  "http://img9.uploadhouse.com/fileuploads/17699/176992615db99bb0fd652a2e6041388b2839a634.png",
-  "http://img4.uploadhouse.com/fileuploads/17699/176992601ca0f28ba4a8f7b41f99ee026d7aaed8.png",
-  "http://img3.uploadhouse.com/fileuploads/17699/17699259cb2d70c6882adc285ab8d519658b5dd7.png",
-  "http://img2.uploadhouse.com/fileuploads/17699/1769925824ea93cbb77ba9e95c1a4cec7f89b80c.png",
-  "http://img7.uploadhouse.com/fileuploads/17699/1769925708af4fb3c954b1d856da1f4d4dcd548a.png",
-  "http://img9.uploadhouse.com/fileuploads/17699/176992568b759acd78f7cbe98b6e4a7baa90e717.png",
-  "http://img9.uploadhouse.com/fileuploads/17699/176992554c2ca340cc2ea8c0606ecd320824756e.png",
-  "http://img5.uploadhouse.com/fileuploads/17699/176992640c06707c66a5c0b08a2549c69745dc2c.png",
-  "http://img6.uploadhouse.com/fileuploads/17699/17699263b01721074bf094aa3bc695aa19c8d573.png",
-  "http://img6.uploadhouse.com/fileuploads/17699/17699262833250fa3063b708c41042005fda437d.png",
-  "http://img9.uploadhouse.com/fileuploads/17699/176992615db99bb0fd652a2e6041388b2839a634.png",
-  "http://img4.uploadhouse.com/fileuploads/17699/176992601ca0f28ba4a8f7b41f99ee026d7aaed8.png",
-  "http://img3.uploadhouse.com/fileuploads/17699/17699259cb2d70c6882adc285ab8d519658b5dd7.png",
-  "http://img2.uploadhouse.com/fileuploads/17699/1769925824ea93cbb77ba9e95c1a4cec7f89b80c.png",
-  "http://img7.uploadhouse.com/fileuploads/17699/1769925708af4fb3c954b1d856da1f4d4dcd548a.png",
-  "http://img9.uploadhouse.com/fileuploads/17699/176992568b759acd78f7cbe98b6e4a7baa90e717.png",
-  "http://img9.uploadhouse.com/fileuploads/17699/176992554c2ca340cc2ea8c0606ecd320824756e.png"
-	];
 
-// tutaj przypisuję funkcję startującą do buttona
+
+// assigning a button to the starting function
 
 document.querySelector(".start").addEventListener('click', function () {
     start();
-    teraz();
+	listenImgs();
 });
 
-function teraz() {
-document.querySelectorAll('.card').forEach(card => card.addEventListener('click', playerAction))};
+// listening for image clicks
+function listenImgs() {
+document.querySelectorAll('img').forEach(image => image.addEventListener('click', playerAction))
+};
 
-// funkcja startująca grę
+// starting function
 //========================
 
 	function start() {
 
-// czyszczenie tablicy
+// cleaning up the board
 		
 		board = document.querySelector('.board');
 		board.innerHTML = '';
-        pictures = [];
-        guesses = [];
-		
-// tutaj do tablicy pictures wrzucam linki do obrazków
+		attempts.innerHTML = 0;
+		hasChecked = false;
 
-		for (let i=0; i < picturesImgs.length; i++) {
-			pictures.push(picturesImgs[i]);	
-		};
-
-// tutaj mieszam tablicę z obrazkami
+// shuffling the array of pictures
 
 		for (var i = pictures.length - 1; i > 0; i--) {
 			var j = Math.floor(Math.random() * (i + 1));
@@ -67,50 +60,50 @@ document.querySelectorAll('.card').forEach(card => card.addEventListener('click'
 			pictures[j] = temp;
 		};
 
-// tutaj wrzucam obrazki do nowo utworzonych divów, żeby je wyświetlić
+// displaying the pictures on the board
 
 		for (let i=0; i < pictures.length; i++) {
 			const card = document.createElement('div');
 			card.classList.add('card');
 			board.appendChild(card);
 			innerSrc = "<img src = '" + pictures[i] + "'/>";
-			card.innerHTML = innerSrc
+			card.innerHTML = innerSrc;
 		}
 		};
 
-
+//===========================
 
 function playerAction() {
 	
 
-	if(guesses.length<2){
-        this.classList.add('rear');
-		guesses.push(this.innerHTML);
-		console.log(guesses);
-        console.log(guesses.length);
-        checkMatch();
-    }
+		this.style.opacity = 1;
+
+		   if (!hasChecked){
+			   hasChecked = true;
+			   firstCard = this;
+		   }
+
+		   else if (hasChecked){
+		   hasChecked = false;
+		   secondCard = this; 
+		   attempts.innerHTML++;
+		   checkMatch();}
+	
+};
+	
+	function checkMatch() {
+		if (firstCard.src === secondCard.src){
+			console.log('bend');
+
+		}
+		else {
+			setTimeout(() => {
+			firstCard.style.opacity = 0;
+			secondCard.style.opacity = 0;
+			}, 1000);
+			
+		}
 
 };
-
-
-function checkMatch() {
-	if (guesses[0] === guesses[1]){
-        console.log('bend');
-		guesses = [];
-    }
-    else if (guesses.length === 2 && guesses[0] !== guesses[1]){
-        guesses = [];
-    }
-};
-
-
-
-
-		
-
-
-
-
 
 
